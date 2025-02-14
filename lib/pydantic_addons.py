d4d00a8e35f52a022f429a1503b763b35e5b0055
Max Huang <letmaxknow@gmail.com>
2025-02-13 21:18:09 -0500

    refactored code, added relative chord relationships

--- lib/pydantic_addons.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, ConfigDict, validate_call
-
-
-strict_validate_call = validate_call(config=ConfigDict(strict=True))
-
-
-class StrictBaseModel(BaseModel):
-    class Config(ConfigDict):
-        strict = True
