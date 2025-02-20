4b9b941b15c5551533f49f82bda05f4794d73251
Max Huang <letmaxknow@gmail.com>
2025-02-20 13:24:38 -0500

    refactored chord voicer

--- components/chord_voicer.py
+++ components/chord_voicer.py
@@ -1,58 +1,95 @@
 from abc import ABC, abstractmethod
-from typing import FrozenSet, List, Dict
+from dataclasses import dataclass
+from typing import FrozenSet, List, Dict, Callable
 
 from components.chord import Chord, ChordQuality
 from components.pitch import Pitch
 
 
+ChordVoicerMemo = Dict[int, FrozenSet[Pitch]]
+
+
+@dataclass(frozen=True)
+class ChordVoicerInstruction:
+    condition: Callable[[int, Chord], bool]  # (index, chord) -> run action?
+    action: Callable[
+        [ChordVoicerMemo, int, Chord], FrozenSet[Pitch]
+    ]  # (memo, index, chord) -> pitch set
+
+
 class ChordVoicer(ABC):
 
+    @classmethod
     @abstractmethod
-    def voice(self, chord_progression: List[Chord]) -> List[FrozenSet[Pitch]]:
+    def get_instructions(
+        cls,
+        chord_progression: List[Chord],
+    ) -> List[ChordVoicerInstruction]:
         pass
+
+    @classmethod
+    def voice(cls, chord_progression: List[Chord]) -> List[FrozenSet[Pitch]]:
+        memo: ChordVoicerMemo = dict()
+        for instruction in cls.get_instructions(chord_progression):
+            for index, chord in enumerate(chord_progression):
+                if instruction.condition(index, chord):
+                    memo[index] = instruction.action(memo, index, chord)
+        return [memo[i] for i in range(len(chord_progression))]
 
 
 class BlockChordVoicer(ChordVoicer):
 
-    def voice(self, chord_progression: List[Chord]) -> List[FrozenSet[Pitch]]:
+    @classmethod
+    def get_instructions(
+        cls,
+        chord_progression: List[Chord],
+    ) -> List[ChordVoicerInstruction]:
 
-        chord_voicings: Dict[int, FrozenSet[Pitch]] = dict()
+        def is_default_chord(index: int, chord: Chord) -> bool:
+            return not is_resolvable_dominant_chord(index, chord)
 
-        def voice_chord(idx: int, chord: Chord):
-
-            if chord.quality == ChordQuality.Dom7:
-                current_tones = {
-                    pitch.chord_tone: pitch for pitch in chord.get_pitches()
-                }
-                next_tones = {
-                    pitch.chord_tone: pitch for pitch in chord_voicings[idx + 1]
-                }
-                chord_voicings[idx] = frozenset(
-                    [
-                        current_tones[3].reoctave_near_pitch(next_tones[1]),
-                        current_tones[7].reoctave_near_pitch(next_tones[3]),
-                        current_tones[1].reoctave_near_pitch(next_tones[5]),
-                    ]
-                )
-                return
-
+        def voice_default_chord(
+            memo: ChordVoicerMemo,
+            index: int,
+            chord: Chord,
+        ) -> FrozenSet[Pitch]:
             target_pitch = Pitch.from_str("D4")
-            pitch_set = frozenset(
+            return frozenset(
                 [
                     pitch.reoctave_near_pitch(target_pitch)
                     for pitch in chord.get_pitches()
                 ]
             )
-            chord_voicings[idx] = pitch_set
 
-        # voice non-dominants
-        for i, chord in enumerate(chord_progression):
-            if chord.quality is not ChordQuality.Dom7:
-                voice_chord(i, chord)
+        def is_resolvable_dominant_chord(index: int, chord: Chord) -> bool:
+            if chord.quality is not ChordQuality.Dom7:  # must be dominant chord
+                return False
+            if index == len(chord_progression) - 1:  # cannot be last chord
+                return False
+            return True
 
-        # voice dominants
-        for i, chord in enumerate(chord_progression):
-            if chord.quality is ChordQuality.Dom7:
-                voice_chord(i, chord)
+        def voice_resolvable_dominant_chord(
+            memo: ChordVoicerMemo,
+            index: int,
+            chord: Chord,
+        ) -> FrozenSet[Pitch]:
+            current_tones = {pitch.chord_tone: pitch for pitch in chord.get_pitches()}
+            next_tones = {pitch.chord_tone: pitch for pitch in memo[index + 1]}
+            return frozenset(
+                [
+                    current_tones[3].reoctave_near_pitch(next_tones[1]),
+                    current_tones[7].reoctave_near_pitch(next_tones[3]),
+                    current_tones[1].reoctave_near_pitch(next_tones[5]),
+                ]
+            )
 
-        return [chord_voicings[i] for i in range(len(chord_progression))]
+        return [
+            ChordVoicerInstruction(
+                condition=is_default_chord,
+                action=voice_default_chord,
+            ),
+            ChordVoicerInstruction(
+                condition=is_resolvable_dominant_chord,
+                action=voice_resolvable_dominant_chord,
+            ),
+        ]
