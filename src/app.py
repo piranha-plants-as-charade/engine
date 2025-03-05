9d85f6ab9379a842f53000161ffaf4d709331ab3
Max Huang <letmaxknow@gmail.com>
2025-03-04 22:34:06 -0500

    Added FastAPI endpoint, updated README (#19)

---
+++ src/app.py
@@ -0,0 +1,45 @@
+import os
+import tempfile
+import dataclasses
+from typing import Dict, Any
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import FileResponse
+
+import main
+
+UPLOAD_DIR = "../inputs"
+
+
+app = FastAPI()
+
+
+@dataclasses.dataclass
+class UploadAudioResponse:
+    upload_path: str
+
+    def as_dict(self) -> Dict[str, Any]:
+        return dataclasses.asdict(self)
+
+
+@app.post("/generate")
+async def generate(file: UploadFile) -> FileResponse:
+
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+    # Save file to disk.
+    upload_file = tempfile.NamedTemporaryFile(
+        dir=UPLOAD_DIR,
+        suffix=".wav",
+        delete=False,
+    )
+    content = await file.read()
+    upload_file.write(content)
+
+    export_path = await main.generate(input_path=upload_file.name)
+
+    upload_file.close()
+
+    return FileResponse(
+        path=export_path,
+        media_type="audio/wav",
+    )
