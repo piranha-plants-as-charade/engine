44f0f214763014739b3cecf9658db47f27c77975
Max Huang <letmaxknow@gmail.com>
2025-03-06 16:07:06 -0500

    added env file

--- src/app.py
+++ src/app.py
@@ -3,14 +3,18 @@ import tempfile
 import dataclasses
 from typing import Dict, Any
 from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 import main
-
-UPLOAD_DIR = "../inputs"
+from env import ENV
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[ENV.FE_BASE_URL],
+)
 
 
 @dataclasses.dataclass
@@ -23,23 +27,25 @@ class UploadAudioResponse:
 
 @app.post("/generate")
 async def generate(file: UploadFile) -> FileResponse:
+    assert file.filename is not None
+    assert file.content_type is not None
 
-    os.makedirs(UPLOAD_DIR, exist_ok=True)
+    ext = os.path.splitext(file.filename)[1]
 
     # Save file to disk.
     upload_file = tempfile.NamedTemporaryFile(
-        dir=UPLOAD_DIR,
-        suffix=".wav",
+        dir=ENV.INPUT_DIR,
+        suffix=ext,
         delete=False,
     )
     content = await file.read()
     upload_file.write(content)
 
-    export_path = await main.generate(input_path=upload_file.name)
+    output_path = await main.generate(input_path=upload_file.name)
 
     upload_file.close()
 
     return FileResponse(
-        path=export_path,
-        media_type="audio/wav",
+        path=output_path,
+        media_type=file.content_type,  # TODO: make this audio/wav
     )
--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
 import os
 import asyncio
 
-EXPORT_DIR = "../outputs"
+from env import ENV
 
 
 # TODO: Create generation process.
 async def generate(input_path: str) -> str:
 
-    os.makedirs(EXPORT_DIR, exist_ok=True)
-    export_path = os.path.join(EXPORT_DIR, os.path.basename(input_path))
+    output_path = os.path.join(ENV.OUTPUT_DIR, os.path.basename(input_path))
 
     await asyncio.sleep(2)
-    os.system(f"cp {input_path} {export_path}")
+    os.system(f"cp {input_path} {output_path}")
 
-    return export_path
+    return output_path
